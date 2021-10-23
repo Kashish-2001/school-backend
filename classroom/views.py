34674214d9dba76d10b1ee38b832f08efe4dc477
Kashish Agrawal <kashish.agrawal1907@gmail.com>
2021-10-23 21:51:12 +0530

    added student create, list, detail, delete APIs

--- classroom/views.py
+++ classroom/views.py
@@ -1,2 +1,27 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView
 from .models import Student, Classroom
+from .serializers import StudentSerializer, ClassroomSerializer
+
+
+class StudentCreateAPIView(CreateAPIView):
+    serializer_class = StudentSerializer
+    model = Student
+    queryset = Student.objects.all()
+
+
+class StudentListAPIView(ListAPIView):
+    serializer_class = StudentSerializer
+    model = Student
+    queryset = Student.objects.all()
+
+
+class StudentDetailAPIView(RetrieveAPIView):
+    serializer_class = StudentSerializer
+    model = Student
+    queryset = Student.objects.all()
+
+
+class StudentDeleteAPIView(DestroyAPIView):
+    serializer_class = StudentSerializer
+    model = Student
+    queryset = Student.objects.all()
